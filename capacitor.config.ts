70b1179f3b107317100c51f5933cbf482c89c88d
Karan Ram <Karanram730005@gmail.com>
2025-12-20 04:36:00 +0530

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cashcompass.app',
+  appName: 'Cash Compass',
+  webDir: 'dist'
+};
+
+export default config;
